f32526ab5caa0bc3c16863c7288ca1cff0db893a
Ankush Yadav <ankushyadav9302@gmail.com>
2021-09-14 03:12:24 +0530

    Add base types for App core state holder

--- types/react-src/App.d.ts
+++ types/react-src/App.d.ts
@@ -1,3 +1,11 @@
-export declare namespace _App {
+import type { _NowPlaying } from "./NowPlaying";
 
+export declare namespace _App {
+    type props = unknown;
+
+    interface state {
+        songInfo: _NowPlaying.SongInfo,
+        volumeOptions: _NowPlaying.VolumeOptions,
+        playbackOptions: _NowPlaying.PlaybackOptions
+    }
 }
