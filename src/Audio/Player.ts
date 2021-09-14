01934d1372699cb8b04ec327e6b0edeebf5bf6d1
Ankush Yadav <ankushyadav9302@gmail.com>
2021-09-15 04:46:12 +0530

    Add option to pass all HowlOptions (instantiation only), add option to set event handlers on some pre-defined events

--- src/Audio/Player.ts
+++ src/Audio/Player.ts
@@ -1,26 +1,44 @@
 import { Howl, HowlOptions } from 'howler';
 
+type HandlerOptions = {
+    load?: undefined | (() => void),
+    loadError?: undefined | (() => void),
+
+    play?: undefined | (() => void),
+    playError?: undefined | (() => void),
+
+    end?: undefined | (() => void)
+}
+
+/**
+ * Wrapper class aroud audio driver (Howler)
+ */
 export default class Player {
     private src: string;
     private howl: Howl;
     private options: HowlOptions;
     private volumeSteps = 15;
+    private handlers: HandlerOptions = { };
 
     constructor(src: string, options: HowlOptions) {
         this.src = src;
         this.options = options;
 
         this.howl = new Howl({
+            ...options,
+
             src: src,
-            loop: options.loop ?? false,
             html5: true,
-            autoplay: options.autoplay ?? false
+            autoplay: false
         });
     }
 
     /* only starts 'howl' if src is different from previous */
     start = (src: string, play = true): Howl => {
         if (src === this.src) { return this.howl }
+
+        /* remove all listeners */
+        this.howl.off();
 
         /* destroy previous 'howls' */
         this.howl.unload();
@@ -33,6 +51,9 @@ export default class Player {
             html5: true,
             autoplay: play
         });
+
+        /* re-register the handlers on the new 'howl' instance */
+        this.registerHandlers();
 
         return this.howl;
     }
@@ -68,4 +89,47 @@ export default class Player {
     getDuration = (): number => this.howl.duration()
 
     state = (): 'unloaded' | 'loading' | 'loaded' => this.howl.state()
+
+    /**
+     * Use for debug purpose only
+     */
+    getDriverInstance = (): Howl => {
+        return this.howl;
+    }
+
+    /**
+     * Use for debug purpose only
+     */
+    getHandlers = (): HandlerOptions => {
+        return this.handlers;
+    }
+
+    setHandlers = (handlers: HandlerOptions, register = false): void => {
+        typeof handlers.load !== 'undefined' ? this.handlers.load = handlers.load : false;
+        typeof handlers.loadError !== 'undefined'
+            ? this.handlers.loadError = handlers.loadError : false;
+
+        typeof handlers.play !== 'undefined' ? this.handlers.play = handlers.play : false;
+        typeof handlers.playError !== 'undefined'
+            ? this.handlers.playError = handlers.playError : false;
+
+        typeof handlers.end !== 'undefined' ? this.handlers.end = handlers.end : false;
+
+        register ? this.registerHandlers() : false;
+    }
+
+    registerHandlers = (): void => {
+        typeof this.handlers.load !== 'undefined'
+            ? this.howl.on('load', this.handlers.load) : false;
+        typeof this.handlers.loadError !== 'undefined'
+            ? this.howl.on('loaderror', this.handlers.loadError) : false;
+
+        typeof this.handlers.play !== 'undefined'
+            ? this.howl.on('play', this.handlers.play) : false;
+        typeof this.handlers.playError !== 'undefined'
+            ? this.howl.on('playerror', this.handlers.playError) : false;
+
+        typeof this.handlers.end !== 'undefined'
+            ? this.howl.on('end', this.handlers.end) : false;
+    }
 }
