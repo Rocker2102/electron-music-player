4612c4e54081044eedb34e35ed996c6d6e370738
Ankush Yadav <ankushyadav9302@gmail.com>
2021-09-21 23:30:07 +0530

    Add dynamic typings to Config.d.ts

--- types/electron/Config.d.ts
+++ types/electron/Config.d.ts
@@ -1,11 +1,11 @@
+type Channel = {
+    [name: string]: string
+}
+
 declare namespace _Electron {
     type Config = {
         'APP_ENV': 'local' | 'development' | 'production',
         'APP_PORT': number,
-        'CHANNELS': {
-            'MAIN': string,
-            'PRIMARY_SYNC': string,
-            'PRIMARY_ASYNC': string
-        }
+        'CHANNELS': Channel
     };
 }
