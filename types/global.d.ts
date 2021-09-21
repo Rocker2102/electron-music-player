6a1825e68d5ebc5c4ab66d8f008e93afbcf1bda2
Ankush Yadav <ankushyadav9302@gmail.com>
2021-09-21 14:24:56 +0530

    Fix compatibility with build-electron command
    -> npm run build-electron was showing namespace conflict if 'globalThis' was used

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,7 +1,10 @@
-declare namespace globalThis {
+/* Add custom type for electronBridge defined by electron (contextBridge) on window object */
+declare global {
     interface Window {
         electronBridge: {
             api: _Electron.api
         }
     }
 }
+
+export {}
