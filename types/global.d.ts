63214d98ba40c2247c9891e8fc0d4198c3e0740e
Ankush Yadav <ankushyadav9302@gmail.com>
2021-09-21 23:34:29 +0530

    Add types to window.electronBridge, ref: 36837439e08d065faa911a8b7a6cff656cf96b61

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,8 +1,10 @@
-/* Add custom type for electronBridge defined by electron (contextBridge) on window object */
+/* Declare custom global types for electronBridge defined by electron (contextBridge) on window object */
 declare global {
     interface Window {
         electronBridge: {
-            api: _Electron.api
+            config: _Electron.Config,
+            api: _Electron.api,
+            getCoverImage: (fileLocation: string) => Promise<string | null>
         }
     }
 }
