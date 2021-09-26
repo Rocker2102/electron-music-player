00931c8828581e2a86a4b84bb585a0044c63c972
Ankush Yadav <ankushyadav9302@gmail.com>
2021-09-26 14:10:30 +0530

    Add prop types to Main.d.ts, MiniDrawer.d.ts

--- types/react-src/MiniDrawer.d.ts
+++ types/react-src/MiniDrawer.d.ts
@@ -1,6 +1,8 @@
 declare namespace _MiniDrawer {
     interface props {
-        main: JSX.Element
+        main: JSX.Element,
+        themeMode: _App.state['common']['theme'],
+        toggleTheme: _Main.props['toggleTheme']
     }
 
     interface state {
