e7173de8b4af7b33b0cc54d0bf62e373dd3a8f6b
Ankush Yadav <ankushyadav9302@gmail.com>
2021-11-02 20:44:13 +0530

    Add simple select music folder alert

--- src/components/Main/MyMusicComponent.tsx
+++ src/components/Main/MyMusicComponent.tsx
@@ -1,15 +1,84 @@
 import React from 'react';
 
-export default class MusicMain extends React.Component {
-    render () {
+import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import AlertTitle from '@mui/material/AlertTitle';
+
+import CloseIcon from '@mui/icons-material/CloseRounded';
+import FolderIcon from '@mui/icons-material/FolderSpecialOutlined';
+
+import type { MyMusicProps, MyMusicState } from '../../types/MyMusicType';
+
+
+interface SelectFolderAlertProps {
+    open: boolean,
+    handleClose: () => void
+    handleSelect: () => void,
+}
+
+const SelectFolderAlert: React.FC<SelectFolderAlertProps> = (props) => {
+    return (
+        <Stack sx={{ width: '100%' }} spacing={2}>
+            <Collapse in={props.open}>
+                <Alert
+                    severity="info"
+                    action={
+                        <IconButton
+                            aria-label="close"
+                            color="inherit"
+                            size="small"
+                            onClick={props.handleClose}
+                        >
+                            <CloseIcon fontSize="inherit" />
+                        </IconButton>
+                    }
+                >
+                    <AlertTitle>
+                        Don&#39;t see all your music?
+                    </AlertTitle>
+
+                    <span onClick={props.handleSelect} style={{ cursor: 'pointer' }}>
+                        Select folders in which to look &nbsp;
+                    </span>
+
+                    <IconButton color="inherit" onClick={props.handleSelect}>
+                        <FolderIcon fontSize="inherit" />
+                    </IconButton>
+                </Alert>
+            </Collapse>
+        </Stack>
+    );
+};
+
+export default class MusicMain extends React.Component
+    <MyMusicProps, MyMusicState> {
+
+    state = {
+        selectFolderAlert: true
+    };
+
+    handleSelectFolder = (): void => {
+        console.log('Selecting folders..');
+    }
+
+    closeSelectFolderAlert = (): void => {
+        this.setState({
+            selectFolderAlert: false
+        });
+    }
+
+    render (): React.ReactNode {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <p>
-                        My Music Component
-                    </p>
-                </header>
-            </div>
+            <Box mt={1} mx={1}>
+                <SelectFolderAlert
+                    open={this.state.selectFolderAlert}
+                    handleClose={this.closeSelectFolderAlert}
+                    handleSelect={this.handleSelectFolder}
+                />
+            </Box>
         );
     }
 }
---
+++ src/types/MyMusicType.ts
@@ -0,0 +1,15 @@
+export interface MyMusicAttr {
+
+}
+
+interface MyMusicHandlers {
+
+}
+
+export interface MyMusicProps extends MyMusicAttr, MyMusicHandlers {
+
+}
+
+export interface MyMusicState {
+    selectFolderAlert: boolean
+}
