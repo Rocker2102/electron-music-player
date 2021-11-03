bb97ccf9fa6a1607f21c08fe13550a8b8bd115f3
Ankush Yadav <ankushyadav9302@gmail.com>
2021-11-03 13:59:21 +0530

    Add simple TabPanel like components for routing inside MyMusicComponent

---
+++ src/components/Main/MyMusic/AlbumsComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Albums extends React.Component {
+    render (): React.ReactNode {
+        return (
+            <div>
+                Albums Component
+            </div>
+        );
+    }
+}
---
+++ src/components/Main/MyMusic/SongsComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Songs extends React.Component {
+    render (): React.ReactNode {
+        return (
+            <div>
+                Songs Component
+            </div>
+        );
+    }
+}
