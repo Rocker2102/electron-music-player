4f143dfbdac137656bd1741333bf9cbe9481c497
Ankush Yadav <ankushyadav9302@gmail.com>
2021-11-24 07:52:27 +0530

    Remove unnecessary test files

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-// import Main from './components/Main/MainComponent';
-
-// test('renders learn react link', () => {
-//   render(<Main />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
